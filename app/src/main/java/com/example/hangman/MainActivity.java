e8bf2c714c9ad7bfc2b82db3c0b1a1d39ed735a6
Alexander Gerasimov <aleksandr.gerasimov.05@gmail.com>
2024-05-09 15:06:38 +0300

    A random word from local server now will be loaded with the application

--- app/src/main/java/com/example/hangman/MainActivity.java
+++ app/src/main/java/com/example/hangman/MainActivity.java
@@ -8,7 +8,20 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.ImageView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class MainActivity extends AppCompatActivity {
+    TextView winlose;
+    String word;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +29,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_hangman); // hangman, not main (!)
 
         TextView guessed = findViewById(R.id.textView);
-        TextView winlose = findViewById(R.id.winlose);
-        winlose.setText("");  // To get rid of Загрузка...
+        winlose = findViewById(R.id.winlose);
         Button btnEnter = findViewById(R.id.btn);
         EditText inp = findViewById(R.id.input);
         ImageView hangman_image = findViewById(R.id.hangman);
+        getWord();
+    }
+
+    public void getWord() {
+        OkHttpClient client = new OkHttpClient();
+        String url = "http://192.168.0.179:8000/one";
+        Request request = new Request.Builder().url(url).build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    throw new IOException("Unexpected code " + response);
+                } else {
+                    try {
+                        JSONObject jsonObject = new JSONObject(response.body().string());
+                        word = jsonObject.get("response").toString();
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    }
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            winlose.setText(word);
+                            //winlose.setText("");  // To get rid of Загрузка...
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
