9f4ebb9efb7b12d02a8341c301069d83490b24ee
Alexander Gerasimov <aleksandr.gerasimov.05@gmail.com>
2024-05-09 14:19:05 +0300

    Added main game activity and activity_hangman.xml

--- app/src/main/java/com/example/hangman/MainActivity.java
+++ app/src/main/java/com/example/hangman/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.hangman;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_hangman); // hangman, not main (!)
+
+        TextView guessed = findViewById(R.id.textView);
+        TextView winlose = findViewById(R.id.winlose);
+        winlose.setText("");  // To get rid of Загрузка...
+        Button btnEnter = findViewById(R.id.btn);
+        EditText inp = findViewById(R.id.input);
+        ImageView hangman_image = findViewById(R.id.hangman);
     }
 }
