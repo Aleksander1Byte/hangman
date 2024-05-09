2849b0160dcd7d31b97fc88aaa8332a895d821c1
Alexander Gerasimov <aleksandr.gerasimov.05@gmail.com>
2024-05-09 15:24:50 +0300

    Implemented a basic system of guessing

--- app/src/main/java/com/example/hangman/MainActivity.java
+++ app/src/main/java/com/example/hangman/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.hangman;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -21,19 +22,77 @@ import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
     TextView winlose;
-    String word;
+    TextView guessed;
+    TextView guessedLettersView;
+    Button btnEnter;
+    EditText inp;
+    ImageView hangman_image;
+    StringBuilder toGuess = new StringBuilder();
+    StringBuilder guessedLetters = new StringBuilder();
+    String wordToGuess, playerInp;
+    String alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+    int cnt = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hangman); // hangman, not main (!)
 
-        TextView guessed = findViewById(R.id.textView);
+        guessed = findViewById(R.id.textView);
+        guessedLettersView = findViewById(R.id.letters);
         winlose = findViewById(R.id.winlose);
-        Button btnEnter = findViewById(R.id.btn);
-        EditText inp = findViewById(R.id.input);
-        ImageView hangman_image = findViewById(R.id.hangman);
+        btnEnter = findViewById(R.id.btn);
+        inp = findViewById(R.id.input);
+        hangman_image = findViewById(R.id.hangman);
         getWord();
+    }
+
+    void setup_rest() {
+        for (int i = 0; i < wordToGuess.length(); i++) {
+            toGuess.append("_ ");
+        }
+        guessed.setText(toGuess);
+        char[] wordToGuessArray = wordToGuess.toCharArray();
+        StringBuilder temp = new StringBuilder();
+        for (char c : wordToGuessArray) {
+            temp.append(c).append(" ");
+        }
+        wordToGuess = temp.toString();
+
+        btnEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean wrongGuess = true;
+                playerInp = inp.getText().toString().toLowerCase();
+
+                for (int i = 0; i < wordToGuess.length(); i++) {
+
+                    if (wordToGuess.charAt(i) == playerInp.charAt(0) && alphabet.contains(playerInp) && guessedLetters.indexOf(playerInp.toUpperCase()) == -1) {
+                        toGuess.setCharAt(i, playerInp.charAt(0));
+                        guessed.setText(toGuess);
+                        wrongGuess = false;
+                    }
+                }
+                if (alphabet.contains(playerInp) && guessedLetters.indexOf(playerInp.toUpperCase()) == -1) {
+                    guessedLetters.append(playerInp.toUpperCase()).append(", ");
+                    guessedLettersView.setText(guessedLetters);
+                }
+
+                if (wrongGuess) {
+                    cnt += 1;
+                    hangman_image.setImageResource(getResources().getIdentifier("main_" + cnt, "drawable", getPackageName()));
+                    if (cnt == 7) {
+                        winlose.setText("Проигрыш!");
+                    }
+                }
+
+                if (toGuess.indexOf("_") == -1) {
+                    winlose.setText("Победа!");
+                }
+
+            }
+
+        });
     }
 
     public void getWord() {
@@ -48,15 +107,15 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     try {
                         JSONObject jsonObject = new JSONObject(response.body().string());
-                        word = jsonObject.get("response").toString();
+                        wordToGuess = jsonObject.get("response").toString();
                     } catch (JSONException e) {
                         throw new RuntimeException(e);
                     }
                     MainActivity.this.runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            winlose.setText(word);
-                            //winlose.setText("");  // To get rid of Загрузка...
+                            winlose.setText("");  // To get rid of Загрузка...
+                            setup_rest();
                         }
                     });
                 }
